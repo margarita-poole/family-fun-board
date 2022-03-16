8f88fd3e929b641b6fa09b4d6e5ef445a6748c12
Margarita Poole <margaritapoole311@gmail.com>
2022-03-15 21:59:37 -0500

    initial commit

---
+++ components/templates/home/HomeTemplate.tsx
@@ -0,0 +1,9 @@
+const HomeTemplate = () => {
+    return (
+      <div id="home" className="w-full flex ">
+          <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      </div>
+    )
+  }
+  
+  export default HomeTemplate
